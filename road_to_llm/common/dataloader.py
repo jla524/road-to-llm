2f9d9893a11796abcdb02e0a5ec57c72740f2830
Jacky Lee <jla524@sfu.ca>
2024-02-29 08:38:35 -0800

    fix: make parent directory

--- road_to_llm/common/dataloader.py
+++ road_to_llm/common/dataloader.py
@@ -8,7 +8,7 @@ from torch.utils.data.distributed import Dataset
 from torchvision import datasets, transforms
 
 ROOTDIR = Path(__file__).parent.parent.parent / "extra" / "datasets"
-ROOTDIR.mkdir(exist_ok=True)
+ROOTDIR.mkdir(parents=True, exist_ok=True)
 
 
 def fetch_mnist(download=True):
