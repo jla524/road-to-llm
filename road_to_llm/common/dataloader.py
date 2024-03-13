447768ad26e79d11c7bcd84cc2b50ba987ead322
Jacky Lee <jla524@sfu.ca>
2024-03-12 23:18:23 -0700

    style: remove duplicate code

--- road_to_llm/common/dataloader.py
+++ road_to_llm/common/dataloader.py
@@ -14,8 +14,9 @@ ROOTDIR.mkdir(parents=True, exist_ok=True)
 
 def fetch_mnist(download=True):
     transform = transforms.Compose([transforms.PILToTensor(), lambda x: x / 255.0])
-    train = datasets.MNIST(ROOTDIR, train=True, transform=transform, target_transform=torch.tensor, download=download)
-    test = datasets.MNIST(ROOTDIR, train=False, transform=transform, target_transform=torch.tensor, download=download)
+    kwargs = {"transform": transform, "target_transform": torch.tensor, "download": download}
+    train = datasets.MNIST(ROOTDIR, train=True, **kwargs)
+    test = datasets.MNIST(ROOTDIR, train=False, **kwargs)
     return train, test
 
 
@@ -24,8 +25,9 @@ def fetch_cifar10(download=True, size=256, crop=224):
         transforms.Resize(size), transforms.CenterCrop(crop), transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-    train = datasets.CIFAR10(ROOTDIR, train=True, transform=transform, target_transform=torch.tensor, download=download)
-    test = datasets.CIFAR10(ROOTDIR, train=False, transform=transform, target_transform=torch.tensor, download=download)
+    kwargs = {"transform": transform, "target_transform": torch.tensor, "download": download}
+    train = datasets.CIFAR10(ROOTDIR, train=True, **kwargs)
+    test = datasets.CIFAR10(ROOTDIR, train=False, **kwargs)
     return train, test
 
 
@@ -40,24 +42,3 @@ def fetch_spamdata(download=True):
         df = pd.read_csv(StringIO(response.text))
         df.to_csv(file_path, index=False)
     return df
-
-
-def fetch_squad(download=True):
-    train_url = "https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json"
-    test_url = "https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json"
-    dataset_path = ROOTDIR / "squad"
-    dataset_path.mkdir(exist_ok=True)
-    dataset = []
-    for url in (train_url, test_url):
-        file_path = dataset_path / url.split("/")[-1]
-        if file_path.exists():
-            with file_path.open("r") as file:
-                data = json.load(file)
-        else:
-            response = requests.get(url)
-            assert response.status_code == HTTPStatus.OK
-            data = json.loads(response.text)
-            with file_path.open("w") as file:
-                json.dump(data, file)
-        dataset.append(data)
-    return dataset
