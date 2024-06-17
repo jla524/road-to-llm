a596d1a1daa8c6eb56940317ed690b04740f7bef
Jacky Lee <jla524@sfu.ca>
2024-06-16 17:16:41 -0700

    style: wrap line

--- road_to_llm/models/resnet.py
+++ road_to_llm/models/resnet.py
@@ -33,7 +33,17 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False, groups=1, width_per_group=64, replace_stride_with_dilation=None, norm_layer=None):
+    def __init__(
+        self,
+        block,
+        layers,
+        num_classes=1000,
+        zero_init_residual=False,
+        groups=1,
+        width_per_group=64,
+        replace_stride_with_dilation=None,
+        norm_layer=None,
+    ):
         super().__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
