d1fc4fd5fa50accecc5a72c7bfec8f263a6cde0e
Jacky Lee <jla524@sfu.ca>
2024-07-16 00:01:08 -0700

    update: mlp

--- examples/mlp_mnist.py
+++ examples/mlp_mnist.py
@@ -1,36 +1,31 @@
-from tqdm import tqdm
-import torch
-from torch import nn, optim
-from torch.utils.data import DataLoader
-from road_to_llm.common.dataloader import fetch_mnist
+from tinygrad import Tensor, nn, TinyJit
+from tinygrad.helpers import getenv, trange
 from road_to_llm.models.mlp import MLP
 
-torch.manual_seed(42)
+Tensor.manual_seed(42)
 
-num_epochs = 10
-batch_size = 128
-learning_rate = 0.005
+X_train, Y_train, X_test, Y_test = nn.datasets.mnist()
+model = MLP()
+opt = nn.optim.Adam(nn.state.get_parameters(model))
 
-train_dataset, test_dataset = fetch_mnist()
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-
-model = MLP(784, 10)
-criterion = nn.CrossEntropyLoss()
-
-for epoch in range(1, num_epochs + 1):
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    model.train()
-    for batch, (data, target) in (t := tqdm(enumerate(train_loader))):
-        optimizer.zero_grad()
-        loss = criterion(model(data), target)
+@TinyJit
+def train_step() -> Tensor:
+    with Tensor.train():
+        opt.zero_grad()
+        samples = Tensor.randint(getenv("BS", 512), high=X_train.shape[0])
+        output = model(X_train[samples])
+        loss = output.sparse_categorical_crossentropy(Y_train[samples])
         loss.backward()
-        optimizer.step()
-        t.set_description(f"{epoch=} {batch=} loss={loss.item():.4f}")
-    model.eval()
-    correct = 0
-    for data, target in test_loader:
-        predictions = model(data).argmax(-1)
-        correct += (predictions == target).sum().item()
-    learning_rate *= 0.7  # yields higher accuracy than weight decay!
-    print(f"test accuracy = {correct / len(test_dataset):.4f}\n")
+        opt.step()
+        return loss
+
+
+@TinyJit
+def get_test_acc() -> Tensor:
+    return (model(X_test).argmax(axis=1) == Y_test).mean() * 100
+
+test_acc = float("nan")
+for i in (t := trange(1000)):
+    loss = train_step()
+    if i % 10 == 9: test_acc = get_test_acc().item()
+    t.set_description(f"loss: {loss.item():6.2f} test_accuracy: {test_acc:5.2f}%")
--- road_to_llm/models/mlp.py
+++ road_to_llm/models/mlp.py
@@ -1,18 +1,17 @@
 """
 https://github.com/geohot/ai-notebooks/blob/master/mnist_from_scratch.ipynb
 """
-from torch import nn, tensor
+from tinygrad import nn, tensor
 
 
-class MLP(nn.Module):
-    def __init__(self, in_features: int, out_features: int, hidden_features: int = 128):
+class MLP:
+    def __init__(self):
         super().__init__()
-        self.in_features = in_features
-        self.l1 = nn.Linear(in_features, hidden_features)
-        self.l2 = nn.Linear(hidden_features, out_features)
+        self.l1 = nn.Linear(784, 128)
+        self.l2 = nn.Linear(128, 10)
 
     def __call__(self, x: tensor) -> tensor:
-        x = x.reshape(-1, self.in_features)
+        x = x.reshape(-1, 784)
         x = self.l1(x).relu()
         x = self.l2(x)
         return x
