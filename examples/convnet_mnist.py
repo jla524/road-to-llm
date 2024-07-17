81eb14e6faad16ddce1d9e8e5d8f634f813f9edf
Jacky Lee <jla524@sfu.ca>
2024-07-16 22:46:58 -0700

    update: spacing

--- examples/convnet_mnist.py
+++ examples/convnet_mnist.py
@@ -25,6 +25,7 @@ def train_step() -> Tensor:
 def get_test_acc() -> Tensor:
     return (model(X_test).argmax(axis=1) == Y_test).mean() * 100
 
+
 test_acc = float("nan")
 for i in (t := trange(70)):
     loss = train_step()
